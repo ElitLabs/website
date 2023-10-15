6b4014cc9d3ea4d09e971b8740aa8753d5a397b7
Lior Pendler <85250652+underscorelior@users.noreply.github.com>
2023-10-14 23:54:05 -0700

    Try and fix favicons

--- src/app/layout.js
+++ src/app/layout.js
@@ -15,7 +15,24 @@ export default function RootLayout({ children }) {
 		<html lang="en">
 			<head>
 				<meta name="description" content={metadata.description} />
-				<link rel="icon" href="/favicon.ico" sizes="any" />
+				<link
+					rel="apple-touch-icon"
+					sizes="180x180"
+					href="/apple-touch-icon.png"
+				/>
+				<link
+					rel="icon"
+					type="image/png"
+					sizes="32x32"
+					href="/favicon-32x32.png"
+				/>
+				<link
+					rel="icon"
+					type="image/png"
+					sizes="16x16"
+					href="/favicon-16x16.png"
+				/>
+				<link rel="manifest" href="/site.webmanifest" />
 			</head>
 			<body className="flex h-screen min-w-full max-w-full flex-col overflow-x-hidden bg-neutral-50 2xl:max-w-[90%]">
 				<Toaster />
