9c92e19f9c65806a03b239cd2fe152f16f090e96
underscorelior <newlioryt@gmail.com>
2023-11-20 01:00:21 -0800

    Create About page

--- src/app/about/page.js
+++ src/app/about/page.js
@@ -4,30 +4,45 @@ export const metadata = {
 
 export default function About() {
 	return (
-		<div className="mx-auto flex w-full max-w-[70%] flex-col items-center py-8">
-			<h1 className="pb-4 text-6xl font-bold">About ElitLabs</h1>
-			<div className="flex flex-row">
-				<p className="gap-y-2 text-xl font-normal"></p>
+		<div className="mx-auto flex w-full flex-col items-center justify-center py-10 lg:max-w-[70%]">
+			<div className="flex flex-col items-center justify-center gap-y-[10vh] pt-4 lg:pt-[2.5vh] 3xl:pt-[12.5vh]">
+				<header className="flex w-full max-w-[90%] lg:max-w-[70%] flex-col items-center justify-center">
+					<h1 className="text-gradient animate-sgradient bg-gradient-to-l from-pink-600 via-purple-600 to-pink-600 bg-500% text-center font-mono text-[3rem] lg:text-[3.125rem] font-black uppercase tracking-wide dark:from-pink-500 dark:via-purple-500 dark:to-pink-500">
+						Our Mission
+					</h1>
+					<h2 className="text-center text-3xl lg:text-[3.25rem] 3xl:text-[3.45rem] font-semibold leading-[3rem]">
+						We aim to help aspiring developers from all backgrounds learn to
+						code
+					</h2>
+				</header>
+				<div className="max-w-[90%] lg:max-w-[65%]">
+					<h1 className="text-center font-mono text-[2.825rem] font-black uppercase tracking-wide">
+						Our Story
+					</h1>
+					<p className="text-center text-2xl 3xl:text-3xl font-medium">
+						We&apos;re ElitLabs, a small group of friends who came together to
+						create a platform for learning and teaching. As students ourselves,
+						we know how hard it can be to learn using the current courses,
+						tutorials, and documentation out there. In our expierence, we
+						realized that we can learn better by
+						<span className="inline font-bold"> doing</span>. That&apos;s why
+						we&apos;re creating a platform for developers to learn and teach
+						through projects. We hope to help aspiring developers from all
+						backgrounds learn to code.
+					</p>
+				</div>
 			</div>
-			<div>
-				<h2 className="text-4xl font-bold">Our Mission</h2>
-				<p className="flex flex-col gap-y-2 text-xl font-normal"></p>
-			</div>
-			<div>
+			{/* <div>
 				<h2 className="text-4xl font-bold">Our Team</h2>
 				<p className="flex flex-col gap-y-2 text-xl font-normal"></p>
 				<p className="flex flex-col gap-y-2 text-xl font-normal">
 					We are currently looking for skilled web developers and tutors! If
 					you&apos;re interested in joining our team, please email us at{' '}
 					<a href="mailto:team@elitlabs.com>">team@elitlabs.com</a>.
-				</p>
+				</p> 
 			</div>
-			<div>
-				<h2 className="text-4xl font-bold">Our Story</h2>
-			</div>
-			<div id="contact">
-				<h2 className="text-4xl font-bold">Contact</h2>
-			</div>
+
+*/}
 		</div>
 	);
 }
