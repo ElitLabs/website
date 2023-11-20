be56863d0fd9eb3c07004af2c5fe6677f9ffe8c5
Lior Pendler <85250652+underscorelior@users.noreply.github.com>
2023-11-20 09:37:27 -0800

    Update gradient and shift around card positions

--- src/app/contact/page.js
+++ src/app/contact/page.js
@@ -1,4 +1,3 @@
-import { FaExternalLinkAlt } from 'react-icons/fa';
 import { SiDiscord, SiInstagram, SiTwitter } from 'react-icons/si';
 
 export const metadata = {
@@ -12,14 +11,14 @@ export default function Contact() {
 				Contact Us
 			</h1>
 
-			<div className="flex max-w-[90%] grid-cols-2 3xl:grid-cols-3 flex-col gap-x-16 gap-y-10 lg:grid lg:max-w-[75%] ">
-				<div className="grid grid-cols-1 justify-between gap-y-4 rounded-md border-1 border-neutral-950 bg-gradient-to-bl from-neutral-100 to-neutral-50 p-6 dark:border-neutral-600 dark:from-neutral-900 dark:to-neutral-950">
+			<div className="flex max-w-[90%] grid-cols-2 flex-col gap-x-16 gap-y-10 lg:grid lg:max-w-[75%] 3xl:grid-cols-3 ">
+				<div className="grid justify-between gap-y-4 rounded-md border-1 border-neutral-950 bg-gradient-to-tr from-neutral-100 via-neutral-50 to-neutral-50 p-6 dark:border-neutral-600 dark:from-neutral-900 dark:via-neutral-950 dark:to-neutral-950 3xl:bg-gradient-to-bl">
 					<h1 className="text-center text-3xl font-semibold">
 						Want to stay up to date with ElitLabs?
 					</h1>
 					<div className="flex grid-cols-2 flex-col gap-x-3 gap-y-4 font-mono text-xl lg:grid">
 						<a
-							className="flex items-center justify-center rounded-md bg-gradient-to-br from-neutral-300 via-neutral-400 to-neutral-300 p-[3px] dark:from-neutral-900 dark:via-neutral-700 dark:to-neutral-900"
+							className="flex items-center justify-center rounded-md bg-gradient-to-tr from-neutral-300 via-neutral-400 to-neutral-300 p-[3px] dark:from-neutral-900 dark:via-neutral-700 dark:to-neutral-900 3xl:bg-gradient-to-br"
 							href="https://instagram.com/elit_labs">
 							<span className="flex w-full flex-col items-center justify-center rounded-md bg-neutral-50 p-3 transition-all duration-300 ease-in-out hover:bg-neutral-100 dark:bg-neutral-950 hover:dark:bg-neutral-900">
 								<SiInstagram className="text-3xl text-[#E1306C]" /> @elit_labs
@@ -34,7 +33,7 @@ export default function Contact() {
 						</a>
 					</div>
 				</div>
-				<div className="grid grid-cols-1 justify-between gap-y-4 rounded-md border-1 border-neutral-950 bg-gradient-to-b from-neutral-100 to-neutral-50 p-6 dark:border-neutral-600 dark:from-neutral-900 dark:to-neutral-950">
+				<div className="row-start-2 mx-auto grid w-full justify-between gap-y-4 rounded-md border-1 border-neutral-950 bg-gradient-to-b from-neutral-100 via-neutral-50 to-neutral-50 p-6 dark:border-neutral-600 dark:from-neutral-900 dark:via-neutral-950 dark:to-neutral-950 lg:col-span-2 lg:w-[50%] 3xl:col-span-1 3xl:w-full">
 					<h1 className="text-center text-3xl font-semibold">
 						For any questions or concerns, please email us at
 					</h1>
@@ -48,7 +47,7 @@ export default function Contact() {
 						</a>
 					</div>
 				</div>
-				<div className="lg:col-span-2 3xl:col-span-1 w-full lg:w-[50%] 3xl:w-full mx-auto flex flex-col justify-between gap-y-4 rounded-md border-1 border-neutral-950 bg-gradient-to-br from-neutral-100 to-neutral-50 p-6 dark:dark:border-neutral-600 dark:from-neutral-900 dark:to-neutral-950">
+				<div className="row-start-1 flex flex-col justify-between gap-y-4 rounded-md border-1 border-neutral-950 bg-gradient-to-tl 3xl:bg-gradient-to-br from-neutral-100 via-neutral-50 to-neutral-50 p-6 dark:dark:border-neutral-600 dark:from-neutral-900 dark:via-neutral-950 dark:to-neutral-950">
 					<h1 className="text-center text-3xl font-semibold">
 						Join our community!
 					</h1>
