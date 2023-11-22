820a0bc0c74e8d12a9199a1e55ce7ba0acc79c26
Lior Pendler <85250652+underscorelior@users.noreply.github.com>
2023-11-22 12:40:09 -0800

    SEO

--- src/app/layout.js
+++ src/app/layout.js
@@ -8,22 +8,24 @@ import { Providers } from './providers';
 import { GeistSans } from 'geist/font/sans';
 import { GeistMono } from 'geist/font/mono';
 
+
 export const metadata = {
 	title: 'ElitLabs',
 	description: 'ElitLabs is a platform to help aspiring developers from all backgrounds learn to code! We teach in ways that are intuitive, comprehensive, and engaging. We here at ElitLabs are big believers in learning by doing.',
-	openGraph: {
-		title: 'ElitLabs',
-		description:
-			'ElitLabs is a platform to help aspiring developers from all backgrounds learn to code! We teach in ways that are intuitive, comprehensive, and engaging. We here at ElitLabs are big believers in learning by doing.',
-		url: 'https://elitlabs.com',
-		type: 'website',
-		images: [
-			{
-				url: '/elitlabsicon.svg',
-				alt: 'ElitLabs',
-			},
-		],
-	},
+	manifest: '/site.webmanifest'
+	// openGraph: {
+	// 	title: 'ElitLabs',
+	// 	description:
+	// 		'ElitLabs is a platform to help aspiring developers from all backgrounds learn to code! We teach in ways that are intuitive, comprehensive, and engaging. We here at ElitLabs are big believers in learning by doing.',
+	// 	url: 'https://elitlabs.com',
+	// 	type: 'website',
+	// 	images: [
+	// 		{
+	// 			url: '/elitlabsicon.svg',
+	// 			alt: 'ElitLabs',
+	// 		},
+	// 	],
+	// },
 };
 
 export default function RootLayout({ children }) {
@@ -33,8 +35,7 @@ export default function RootLayout({ children }) {
 			className={`${GeistSans.variable} ${GeistMono.variable} scroll-smooth`}
 			suppressHydrationWarning>
 			<head>
-				<meta name="description" content={metadata.description} />
-				<link
+				{/* <link
 					rel="apple-touch-icon"
 					sizes="180x180"
 					href="/images/apple-touch-icon.png"
@@ -51,7 +52,7 @@ export default function RootLayout({ children }) {
 					sizes="16x16"
 					href="/images/favicon-16x16.png"
 				/>
-				<link rel="manifest" href="/site.webmanifest" />
+				<link rel="manifest" href="/site.webmanifest" /> */}
 			</head>
 			<body className="flex h-screen min-w-full max-w-full flex-col overflow-x-hidden scroll-smooth bg-neutral-50 font-sans dark:bg-neutral-950">
 				<Providers>
